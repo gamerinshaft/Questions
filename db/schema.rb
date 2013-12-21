11b889e714c09197fa81af125facd18b9cb8c1a1
taneichijunpei@gamerinshaft <j.tane3ma@gmail.com>
2013-12-21 15:51:19 +0900

    b_question setting

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131221053820) do
+ActiveRecord::Schema.define(version: 20131221064905) do
+
+  create_table "b_questions", force: true do |t|
+    t.text     "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "m_answers", force: true do |t|
     t.boolean  "check"
