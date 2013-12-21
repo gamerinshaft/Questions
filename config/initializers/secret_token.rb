fd6aa4d9bbe567d87c7426969a121ede2ff76201
taneichijunpei@gamerinshaft <j.tane3ma@gmail.com>
2013-12-21 14:10:17 +0900

    initialcommit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kaitou::Application.config.secret_key_base = '742d2d6c210f0c0f9f0550578e20bdf148e68175e311a54011f491c96d30e25d042d3b1ddf86c054e414cac9445d3ccb67bbd9a6fa6274a7bd204a81ee8ff593'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Kaitou::Application.config.session_store :cookie_store, key: '_kaitou_session'
