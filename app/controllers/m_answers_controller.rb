59ee1e5d86a1ec98ab4f82c33e42edafccfdcb08
taneichijunpei@gamerinshaft <j.tane3ma@gmail.com>
2013-12-21 15:39:39 +0900

    controllerをみじかくした

--- app/controllers/m_answers_controller.rb
+++ app/controllers/m_answers_controller.rb
@@ -1,35 +1,30 @@
 class MAnswersController < ApplicationController
   before_action :set_m_answer, only: [:show, :edit, :update, :destroy]
-
+  before_action :set_m_question, except: [:destroy]
   # GET /m_answers
   # GET /m_answers.json
   def index
     @m_answers = MAnswer.all
-    @m_question = MQuestion.find(params[:m_question_id])
   end
 
   # GET /m_answers/1
   # GET /m_answers/1.json
   def show
-    @m_question = MQuestion.find(params[:m_question_id])
   end
 
   # GET /m_answers/new
   def new
-    @m_question = MQuestion.find(params[:m_question_id])
     @m_answer = MAnswer.new
   end
 
   # GET /m_answers/1/edit
   def edit
-    @m_question = MQuestion.find(params[:m_question_id])
     @m_answer = @m_question.m_answers.find(params[:id])
   end
 
   # POST /m_answers
   # POST /m_answers.json
   def create
-    @m_question = MQuestion.find(params[:m_question_id])
     @m_answer = @m_question.m_answers.build(m_answer_params)
     respond_to do |format|
       if @m_answer.save
@@ -45,7 +40,6 @@ class MAnswersController < ApplicationController
   # PATCH/PUT /m_answers/1
   # PATCH/PUT /m_answers/1.json
   def update
-    @m_question = MQuestion.find(params[:m_question_id])
     @m_answer = @m_question.m_answers.build(m_answer_params)
     respond_to do |format|
       if @m_answer.update(m_answer_params)
@@ -61,6 +55,7 @@ class MAnswersController < ApplicationController
   # DELETE /m_answers/1
   # DELETE /m_answers/1.json
   def destroy
+
     @m_answer.destroy
     respond_to do |format|
       format.html { redirect_to m_question_m_answers_url }
@@ -77,5 +72,9 @@ class MAnswersController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def m_answer_params
       params.require(:m_answer).permit(:check, :answer)
+    end
+
+    def set_m_question
+      @m_question = MQuestion.find(params[:m_question_id])
     end
 end
