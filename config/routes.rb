ef34647d62e79f0e4a84071ec330ccd9e9afe746
taneichijunpei@gamerinshaft <j.tane3ma@gmail.com>
2013-12-21 14:32:52 +0900

    migration database

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Kaitou::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'm_questions#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
